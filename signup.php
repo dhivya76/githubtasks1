37cb5cd3c10ddf37fb6211461ef038959aa38836
dhivya76 <dhivyaruby76@gmail.com>
2026-05-20 08:11:36 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <h2>Signup Page</h2>
+    <form action="">
+        <label>First Name</label>
+        <input type="text" name="f_name" value="">
+        <label>Password</label>
+        <input type="password" name="pwd" id="pwd" value="">
+        <button value="submit">Submit</button>
+    </form>
+</body>
+</html>

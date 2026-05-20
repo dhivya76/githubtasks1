16c960ac561b8cf39cd11358781698a4ebb30388
dhivya76 <dhivyaruby76@gmail.com>
2026-05-20 08:13:36 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Signup</title>
 </head>
 <body>
     <h2>Signup Page</h2>
